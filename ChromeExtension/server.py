2c5232c000da07d19091ea073c33e61358395090
mbrenn <mbrenn@vt.edu>
2022-02-26 15:09:00 -0500

    working server code for python html interfacing

---
+++ ChromeExtension/server.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+import requests
+import json
+import os
+from PIL import Image
+
+def get_data(api_key):
+    raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}').text
+    response = json.loads(raw_response)
+    return response
+
+def get_date(response):
+    date = response['date']
+    return date
+
+def get_url(response):
+    url = response['url']
+    return url
+
+def download_image(url, date):
+    if os.path.isfile(f'{date}.png') == False:
+        raw_image = requests.get(url).content
+        with open(f'{date}.jpg', 'wb') as file:
+            file.write(raw_image)
+    else:
+        return FileExistsError
+        
+def getNewPicture():
+    print("yo")
+    response = get_data("L1rCrYxdmYFpywxIkOoN0Q0fNJ8pTG3NZEyGAuRk")
+    download_image(get_url(response), get_date(response))
+    url = get_url(response)
+    return url
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+#C:/Users/mattb/Documents/VTHacks2022/VTHacks2022/nasaPictures.py
+@app.route('/my-link/')
+def my_link():
+  return getNewPicture()
+
+if __name__ == '__main__':
+  app.run(debug=True)
